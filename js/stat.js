a26582788b5bf7f31b1057853855aa85c0a49e19
serrrgun <serrrgun@mail.ru>
2018-02-02 19:55:24 +0200

    Добавляет файл stat.js + начало гистагораммы

---
+++ js/stat.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+window.renderStatistics = function (ctx) {
+  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, 100, 10, '#fff');
+
+  ctx.fillStyle = '#000000';
+  ctx.font = '16px PT Mono';
+
+  ctx.fillText('Ура, вы победили!', 130, 40);
+  ctx.fillText('Список результатов:', 130, 40);
+};
