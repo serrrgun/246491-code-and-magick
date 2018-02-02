86dc3c6bdc259845a5dee5b7d9c4d7dc3333a045
serrrgun <serrrgun@mail.ru>
2018-02-02 21:29:01 +0200

    Начало гистограммы

--- js/stat.js
+++ js/stat.js
@@ -8,7 +8,16 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-window.renderStatistics = function (ctx) {
+var getArrayMaxElementIndex = function (array) {
+
+  var max = -1
+  for (var i = 0; i < array.length; i++) {
+    max = Math.max(array[i], max);
+  }
+  return max;
+};
+
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, 100, 10, '#fff');
 
@@ -16,5 +25,27 @@ window.renderStatistics = function (ctx) {
   ctx.font = '16px PT Mono';
 
   ctx.fillText('Ура, вы победили!', 130, 40);
-  ctx.fillText('Список результатов:', 130, 40);
+  ctx.fillText('Список результатов:', 130, 60);
+
+  var max = getArrayMaxElementIndex(times);
+
+  var histogramHeight = 150;
+  var step = histogramHeight / max;
+
+  var columnWidth = 40;
+  var cloudHeight = 270;
+  var initialY = 20;
+  var initialX = 130;
+  var indent = 50;
+
+  for (var i = 0; i < names.length; i++) {
+
+    var columnHeight = times[i] * step;
+
+    ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    }
+    ctx.fillRect(initialX + (columnWidth + indent) * i, cloudHeight - columnHeight - initialY, columnWidth, columnHeight);
+  }
 };
